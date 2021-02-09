4c81aa1ec1e683743aeacde4870de2ebc42d7dd4
Jayesh Karathiya <jayeshsagar@hotmail.com>
2021-02-09 07:52:40 +0000

    More checks in sorting array example

--- SortingArray/JavaScript/solution.js
+++ SortingArray/JavaScript/solution.js
@@ -1,19 +1,25 @@
 function run(unsortedInput) {
-    try {
-        let strArray = unsortedInput.split(" ").map((char) => {
-            let num = parseInt(char, 10);
-            if (isNaN(num) && !isFinite(num)) {
-                throw "Error with sorting";
-            }
-            return num;
-        });
 
-        let sortedOutput = strArray.sort((a, b) => a - b)
-        return sortedOutput.join(" ");
+    let error = "Error with sorting";
+
+    if (typeof unsortedInput === 'string' || val instanceof String) {
+        try {
+            let strArray = unsortedInput.split(" ").map((char) => {
+                let num = parseInt(char, 10);
+                if (isNaN(num) && !isFinite(num)) {
+                    throw error;
+                }
+                return num;
+            });
+
+            let sortedOutput = strArray.sort((a, b) => a - b)
+            return sortedOutput.join(" ");
+        }
+        catch (err) {
+            return err;
+        }
     }
-    catch (err) {
-        return err;
-    }
+    return error;
 }
 
 module.exports = run;
