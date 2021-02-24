c215c43d6306c6b599e9c6d7b4964ee57fb8e631
Jayesh Karathiya <jayeshsagar@hotmail.com>
2021-02-24 07:34:29 +0000

    refactor Maximum Possible Value

--- MaximumPossibleValue/CSharp/MaximumPossibleValue.cs
+++ MaximumPossibleValue/CSharp/MaximumPossibleValue.cs
@@ -4,14 +4,15 @@ public class Solution
     public int MaximumPossibleValue(int n)
     {
         int abs = n >= 0 ? 1 : -1;
-        int max = -8000;
-        var str = Math.Abs(n).ToString();
+        int maxValue = -8000;
+        string additionalDigit = "5";
+        string str = Math.Abs(n).ToString();
         for (int i = 0; i < str.Length; i++)
         {
-            int val = abs * Int32.Parse(str.Substring(0, i) + "5" + str.Substring(i, str.Length - i));
-            if (val > max)
-                max = val;
+            int currentValue = abs * Int32.Parse(str.Substring(0, i) + additionalDigit + str.Substring(i, str.Length - i));
+            if (currentValue > maxValue)
+                maxValue = currentValue;
         }
-        return max;
+        return maxValue;
     }
 }
